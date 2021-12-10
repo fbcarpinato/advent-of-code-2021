bb9baa3f8e406c79ec25dfbe11583a00ee5fcba4
Fabio Carpinato <fabiocarpinato@outlook.it>
2021-12-10 13:17:14 +0100

    solves day 1 challenge

--- src/day-1/day-1.test.ts
+++ src/day-1/day-1.test.ts
@@ -1,7 +1,11 @@
 import solve from './day-1';
 
 describe('day 1', () => {
-  it('should workd', () => {
-    expect(solve()).toBe(1);
+  it('test case 1', () => {
+    expect(solve('./test-cases/test-case-1.txt')).toBe(4);
+  });
+
+  it('test case 2', () => {
+    expect(solve('./test-cases/test-case-2.txt')).toBe(1);
   });
 });
--- src/day-1/day-1.ts
+++ src/day-1/day-1.ts
@@ -1,3 +1,30 @@
-export default function solve() {
-  return 1;
+import * as fs from 'fs';
+import * as path from 'path';
+
+export default function solve(filePath: string) {
+  const file = fs.readFileSync(path.join(__dirname, filePath), 'utf-8');
+
+  const measurements = file
+    .split('\n')
+    .filter((m) => m)
+    .map(Number);
+
+  let referenceMeasurement: number | null = null;
+
+  let increasingMeasurements = 0;
+
+  measurements.forEach((measurement) => {
+    if (!referenceMeasurement) {
+      referenceMeasurement = measurement;
+      return;
+    }
+
+    if (measurement > referenceMeasurement) {
+      increasingMeasurements += 1;
+    }
+
+    referenceMeasurement = measurement;
+  });
+
+  return increasingMeasurements;
 }
